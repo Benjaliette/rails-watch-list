0a395cb61c646410da155e81832eab87893d3a39
Benjamin Liet <liet.ben@gmail.com>
2022-05-19 12:54:08 +0200

    Added a seed with top films and TMDB API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts "Deleted all instances !"
+
+Movie.destroy_all
+
+puts "Deleted!"
+
+puts "Adding the top movies from TMDB"
+
+URL = "https://tmdb.lewagon.com/movie/top_rated"
+
+movies = JSON.parse(URI(URL).read)
+
+movies["results"].each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w100#{movie['poster_path']}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "Added"
